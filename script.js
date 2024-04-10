93516dd1c618e2073a9d8f1e4f38ff5b2e251972
Art Singletary <artsingletary@yahoo.com>
2024-04-10 19:33:14 -0400

    Displaying and Hiding the Form

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ btn.addEventListener("click", function () {
   //console.log("CLICK");
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
+    btn.textContent = "Close";
   } else {
     form.classList.add("hidden");
+    btn.textContent = "Share a fact";
   }
 });
