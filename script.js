2e0137c096eb53d9db07c9650980a3ba6dcf5ead
Art Singletary <artsingletary@yahoo.com>
2024-04-14 08:48:22 -0400

    Creating DOM Elements

--- script.js
+++ script.js
@@ -1,16 +1,94 @@
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+//Selecting DOM elements
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
 
-btn.addEventListener("click", function () {
-  //console.log("CLICK");
-  if (form.classList.contains("hidden")) {
-    form.classList.remove("hidden");
-    btn.textContent = "Close";
-  } else {
-    form.classList.add("hidden");
-    btn.textContent = "Share a fact";
-  }
-});
+// Create DOM elements: Render facts in list
+factsList.innerHTML = "";
+createFactsList(initialFacts);
+
+function createFactsList(dataArray) {
+  //factsList.insertAdjacentHTML("afterbegin", "<li>art</li>");
+
+  const htmlArr = dataArray.map(
+    (fact) => `<li class="fact">
+    <p>
+      ${fact.text}
+      <a
+        class="source"
+        href="${fact.source}"
+        target="_blank"
+        >(Source)</a>
+    </p>
+    <span class="tag"
+       style="background-color: 
+       #3b82f6">${fact.category}</span>
+    </li>`
+  );
+
+  console.log(htmlArr);
+  const html = htmlArr.join("");
+  factsList.insertAdjacentHTML("afterbegin", html);
+
+  // Toggle form visability
+  btn.addEventListener("click", function () {
+    if (form.classList.contains("hidden")) {
+      form.classList.remove("hidden");
+      btn.textContent = "Close";
+    } else {
+      form.classList.add("hidden");
+      btn.textContent = "Share a fact";
+    }
+  });
+}
+
+console.log("Yello");
+console.dir(btn);
 
 /*
 
@@ -24,15 +102,6 @@ console.log("Upvotes", totalUpvotes);
 let votesFalse = 4;
 const isCorrect = votesFalse < totalUpvotes;
 console.log(isCorrect);
-*/
-
-function calcFactAge(year) {
-  const currentYear = new Date().getUTCFullYear();
-  const age = currentYear - year;
-
-  if (age >= 0) return age;
-  else return `Impossible year. Year needs to be less or equal ${currentYear}`;
-}
 
 const age1 = calcFactAge(2015);
 console.log(age1);
@@ -49,7 +118,8 @@ const calcFactAge2 = (year) =>
     : `impossible year.  Year needs to be less or 
  equal ${new Date().getFullYear()}`;
 
-/*
+console.log("hello worldddddd");
+
 let votesInteresting = 20;
 let votesMindblowing = 23;
 
@@ -92,20 +162,86 @@ console.log(upperText);
 const str = `The current fact is "${text}" is ${calcFactAge(2015)} years old.
 It is probably ${totalUpvotes > votesFalse ? "correct" : "not true"}.`;
 console.log(str);
-*/
+
 
 // Array with a string, integer and boolean.
 const fact = ["Lisbon is the capital of Portuagal", 2015, true];
 
-console.log(fact);
-console.log(fact[0]);
-console.log(fact[0].length);
+console.log(fact[2]);
+console.log(fact.length);
+console.log(fact[fact.length - 1]);
 
-const [text, createdIn, isCorrect] = fact;
-console.log(createdIn);
+const [text, createdIn] = fact;
+const newfact = [...fact, "society"];
+console.log(newfact);
+
+[2, 4, 6, 8].forEach(function (num) {
+  console.log(num);
+});
+
+const times10 = [2, 4, 6, 8].map(function (num) {
+  return num * 10;
+});
+console.log(times10);
+
+const times11 = [2, 4, 6, 8].map((num) => num * 11);
+console.log(times11);
+
+// Array of objects
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const allCategories = CATEGORIES.map((el) => el.name);
+console.log(allCategories);
+
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+
+  if (age >= 0) return age;
+  else
+    return `Impossible year. Year needs to be less
+   or equal ${currentYear}`;
+}
+
+const factAges = initialFacts.map((el) => calcFactAge(el.createdIn));
+console.log(factAges);
+
+// LINK TO APP SAMPLE DATA: https://docs.google.com/spreadsheets/d/1eeldcA_OwP4DHYEvjG0kDe0cRys-cDPhc_E9P9G1e3I/edit#gid=0
 
 // Spreading, taking all elements from the fact array and adding them to
 // newfact and then adding another value.
 // You can only do this operation inside an array.
-const newfact = [...fact, "society"];
-console.log(newfact);
+
+
+// Text is a property of the factobj
+const factObj = {
+  text: "Listbon is the capital of Portugal",
+  category: "society",
+  createdIn: 2015,
+  isCorrect: true,
+  createSummary: function () {
+    return `the fact ${
+      this.text
+    }" is from category ${this.category.toUpperCase()}`;
+  },
+};
+
+console.log(factObj.text);
+console.log(factObj["text"]);
+
+const { category, isCorrect } = factObj;
+console.log(category);
+console.log(factObj.createSummary());
+
+console.log("theis");
+*/
